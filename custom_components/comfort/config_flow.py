ad322012449211926fea199551cf55e3865de595
Jon Attwood <jon@attwoodfamily.uk>
2025-09-10 16:26:46 +0000

    .

--- custom_components/comfort/config_flow.py
+++ custom_components/comfort/config_flow.py
@@ -4,9 +4,10 @@ from __future__ import annotations
 
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.const import CONF_USERNAME
+from homeassistant.const import CONF_PASSWORD
 from homeassistant.helpers import selector
-from homeassistant.helpers.aiohttp_client import async_create_clientsession
+
+# from homeassistant.helpers.aiohttp_client import async_create_clientsession
 from slugify import slugify
 
 from .api import (
@@ -31,29 +32,33 @@ class ComfortFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         _errors = {}
         if user_input is not None:
             try:
-                await self._system_details(
-                    pin=user_input[CONF_USERNAME],
+                await self._system(
+                    pin=user_input[CONF_PASSWORD],
                 )
-            finally:
-                await self.async_set_unique_id(
-                    ## Do NOT use this in production code
-                    ## The unique_id should never be something that can change
-                    ## https://developers.home-assistant.io/docs/config_entries_config_flow_handler#unique-ids
-                    unique_id=slugify(user_input[CONF_USERNAME])
-                )
-                self._abort_if_unique_id_configured()
-                return self.async_create_entry(
-                    title=user_input[CONF_USERNAME],
-                    data=user_input,
-                )
+            except ComfortApiClientError as exception:
+                LOGGER.exception(exception)
+                _errors["base"] = "unknown"
+        else:
+            await self.async_set_unique_id(
+                ## Do NOT use this in production code
+                ## The unique_id should never be something that can change
+                ## https://developers.home-assistant.io/docs/config_entries_config_flow_handler#unique-ids
+                ## unique_id=slugify(user_input[CONF_PASSWORD])
+                unique_id="jon123456789"
+            )
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(
+                title="If only this worked!",
+                data=user_input,
+            )
 
         return self.async_show_form(
             step_id="system",
             data_schema=vol.Schema(
                 {
                     vol.Required(
-                        CONF_USERNAME,
-                        default=(user_input or {}).get(CONF_USERNAME, vol.UNDEFINED),
+                        CONF_PASSWORD,
+                        default=(user_input or {}).get(CONF_PASSWORD, vol.UNDEFINED),
                     ): selector.TextSelector(
                         selector.TextSelectorConfig(
                             type=selector.TextSelectorType.TEXT,
@@ -69,6 +74,7 @@ class ComfortFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             errors=_errors,
         )
 
-    async def _system_details(self, pin: str) -> None:
+    async def _system(self, pin: str) -> None:
         """Get system details."""
-        print("Comfort System Details For now..." + pin + ".")
+        pin = pin
+        LOGGER.error("Comfort System Details For now..." + pin + ".")
