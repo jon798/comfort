4588bd9555ecc30524832c155e78e8c4d8518bb9
Jon Attwood <jon@attwoodfamily.uk>
2025-09-13 15:04:50 +0000

    .

--- custom_components/comfort/comfortsys.py
+++ custom_components/comfort/comfortsys.py
@@ -47,7 +47,7 @@ class ComfortSystem:
         # In a real implementation, this would query the hub to find the devices.
         self.comfortsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("connecting to " + ip + " " + str(port))
-        self.comfortsock.connect((ip, port))
+        self.comfortsock.connect((ip, int(port)))
         self.comfortsock.settimeout(comforttimeout)
         self.login(pin)
         # end connection bit
