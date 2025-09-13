391d850e909139f2025f0ba5484899b4b5e2ccd2
Jon Attwood <jon@attwoodfamily.uk>
2025-09-13 09:03:08 +0000

    .

--- custom_components/comfort/__init__.py
+++ custom_components/comfort/__init__.py
@@ -7,92 +7,40 @@ https://github.com/ludeeus/integration_blueprint
 
 from __future__ import annotations
 
-from datetime import timedelta
 from typing import TYPE_CHECKING
 
-from homeassistant.const import (
-    CONF_PASSWORD,
-    CONF_USERNAME,
-    Platform,
-)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.loader import async_get_loaded_integration
-
-from .api import ComfortApiClient
-from .const import (
-    COMFORT_IP,
-    COMFORT_PIN,
-    COMFORT_PORT,
-    COMFORT_TIMEOUT,
-    COMFORT_RETRY,
-    DOMAIN,
-    LOGGER,
-)
-from .coordinator import ComfortDataUpdateCoordinator
-from .data import ComfortData
+from . import comfort
 
 if TYPE_CHECKING:
     from homeassistant.core import HomeAssistant
 
-    from .data import ComfortConfigEntry
+# List of platforms to support. There should be a matching .py file for each,
+# eg <cover.py> and <sensor.py>
+PLATFORMS = [Platform.SENSOR, Platform.BINARY_SENSOR, Platform.SWITCH]
 
-PLATFORMS: list[Platform] = [
-    Platform.SENSOR,
-    Platform.BINARY_SENSOR,
-    Platform.SWITCH,
-]
+type ComfortConfigEntry = ConfigEntry[comfort.comfort]
 
 
-# https://developers.home-assistant.io/docs/config_entries_index/#setting-up-an-entry
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ComfortConfigEntry,
-) -> bool:
-    """Set up this integration using UI."""
-    coordinator = ComfortDataUpdateCoordinator(
-        hass=hass,
-        logger=LOGGER,
-        name=DOMAIN,
-        config_entry=entry,
-        my_api=ComfortApiClient(
-            pin=entry.data[COMFORT_PIN],
-            ip=entry.data[COMFORT_IP],
-            port=entry.data[COMFORT_PORT],
-            timeout=timedelta(seconds=entry.data[COMFORT_TIMEOUT]),
-            retry=entry.data[COMFORT_RETRY],
-        ),
-    )
-    entry.runtime_data = ComfortData(
-        pin=entry.data[COMFORT_PIN],
-        ip=entry.data[COMFORT_IP],
-        port=entry.data[COMFORT_PORT],
-        timeout=entry.data[COMFORT_TIMEOUT],
-        retry=entry.data[COMFORT_RETRY],
-        integration=async_get_loaded_integration(hass, entry.domain),
-        coordinator=coordinator,
-    )
+async def async_setup_entry(hass: HomeAssistant, entry: ComfortConfigEntry) -> bool:
+    """Set up Hello World from a config entry."""
+    # Store an instance of the "connecting" class that does the work of speaking
+    # with your actual devices.
+    entry.runtime_data = comfort.comfort(hass, entry.data["host"])
 
-    # https://developers.home-assistant.io/docs/integration_fetching_data#coordinated-single-api-poll-for-data-for-all-entities
-    await coordinator.async_config_entry_first_refresh()
-
+    # This creates each HA object for each platform your device requires.
+    # It's done by calling the `async_setup_entry` function in each platform module.
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
-
     return True
 
 
-async def async_unload_entry(
-    hass: HomeAssistant,
-    entry: ComfortConfigEntry,
-) -> bool:
-    """Handle removal of an entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    # This is called when an entry/configured device is to be removed. The class
+    # needs to unload itself, and remove callbacks. See the classes for further
+    # details
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
-
-async def async_reload_entry(
-    hass: HomeAssistant,
-    entry: ComfortConfigEntry,
-) -> None:
-    """Reload config entry."""
-    await hass.config_entries.async_reload(entry.entry_id)
+    return unload_ok
