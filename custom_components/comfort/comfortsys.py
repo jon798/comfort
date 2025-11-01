1ada87c2ba1fd58b9a0f49e088d4902a428ac4f6
Jon Attwood <jon@attwoodfamily.uk>
2025-11-01 10:22:00 +0000

    .

--- custom_components/comfort/comfortsys.py
+++ custom_components/comfort/comfortsys.py
@@ -67,8 +67,7 @@ class ComfortSystem:
         print("Sent:", ("\x03LI" + pin + "\r").encode())
         # threading.Thread(target=self.worker(), daemon=True).start()
 
-    @callback
-    def readlines(self):
+    async def readlines(self):
         print("Is this function ever getting called? How do I do that?")
         self.comfortsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         delim = "\r"
